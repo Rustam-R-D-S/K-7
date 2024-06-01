3bbd8035e81369b2f503b61302b1d1e88d18bcca
Rustam-R-D-S <https://github.com/Rustam-R-D-S/home-work-51.git>
2024-06-01 13:57:58 +0600

    created new component Buttons with an array with objects for the buttons and typed them

---
+++ src/components/Buttons/Buttons.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {food} from "../../types.ts";
+
+
+
+const menu: food[] = [
+    { name: 'Shawurma', price:220},
+    { name: 'Dolma', price: 300},
+    { name: 'Salad', price: 120},
+    { name: 'Lagman', price: 270},
+    { name: 'Samsa', price: 65},
+    { name: 'Plov', price: 500},
+    { name: 'Tea', price: 20},
+    { name: 'Cola', price: 50}
+];
+console.log(menu)
+
+const Buttons = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Buttons;
