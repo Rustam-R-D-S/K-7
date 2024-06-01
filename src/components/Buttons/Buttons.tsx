9b19178a6e65860603203891e97ba07126ef8696
Rustam-R-D-S <https://github.com/Rustam-R-D-S/home-work-51.git>
2024-06-01 18:13:49 +0600

    add getTotalPrice, removeFood, add if else in leftSide and some css

--- src/components/Buttons/Buttons.tsx
+++ src/components/Buttons/Buttons.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {food} from "../../types.ts";
+import {menuItem, Food} from "../../types.ts";
 import imageShawurma from "../../assets/images/item_185.jpg"
 import imageDolma from "../../assets/images/Без названия.jpeg"
 import imageSalad from "../../assets/images/images (1).jpeg"
@@ -10,7 +10,7 @@ import imageTea from "../../assets/images/Без названия (1).jpeg"
 import imageCola from "../../assets/images/Без названия (2).jpeg"
 
 
-const menu: food[] = [
+ export const menu: menuItem[] = [
     { name: 'Shawurma', price:220, image:imageShawurma},
     { name: 'Dolma', price: 300, image: imageDolma},
     { name: 'Salad', price: 120, image:imageSalad},
@@ -22,34 +22,75 @@ const menu: food[] = [
 ];
 
 
-
 const Buttons = () => {
-    const [food, setFood] = useState([
-        { name: 'Shawurma', count: 0 },
-        { name: 'Dolma', count: 0 },
-        { name: 'Salad', count: 0 },
-        { name: 'Lagman', count: 0 },
-        { name: 'Somsa', count: 0 },
-        { name: 'Plov', count: 0 },
-        { name: 'Tea', count: 0 },
-        { name: 'Cola', count: 0 }
+    const [foods, setFoods] = useState<Food[]>([
+        { name: 'Shawurma', count: 0, price: 0 },
+        { name: 'Dolma', count: 0, price: 0 },
+        { name: 'Salad', count: 0, price: 0 },
+        { name: 'Lagman', count: 0, price: 0},
+        { name: 'Somsa', count: 0, price: 0},
+        { name: 'Plov', count: 0, price: 0 },
+        { name: 'Tea', count: 0, price: 0 },
+        { name: 'Cola', count: 0, price: 0 }
     ]);
 
-    const addFood = (name:string) => {
-        setFood(prevFood =>
-            prevFood.map(food =>
-                food.name === name ? { ...food, count: food.count + 1 } : food
-            )
-        );
+    const addFood = (name: string) => {
+        const foodToAdd = menu.find(item => item.name === name);
+        if (foodToAdd) {
+            setFoods(prevFood =>
+                prevFood.map(food =>
+                    food.name === name ? { ...food, count: food.count + 1, price: food.price + foodToAdd.price } : food
+                )
+            );
+        }
     }
-    console.log(food)
+
+    const removeFood = (name: string) => {
+        const foodToAdd = menu.find(item => item.name === name);
+        if (foodToAdd) {
+            setFoods(prevFoods =>
+                prevFoods.map(food =>
+                    food.name === name ? { ...food, count: Math.max(food.count - 1, 0), price: Math.max(food.price - foodToAdd.price, 0) } : food
+                )
+            );
+        }
+    }
+
+    const getTotalPrice = () => {
+        return foods.reduce((acc, food) => acc + (food.price), 0);
+    }
+
+
     return (
-        <div>
-            {menu.map( food => (
-                <div key={food.name} className="buttons">
-                    <button onClick={() => addFood(food.name)}>{food.name} <br/>{food.price} сом <img src={food.image} /></button>
+        <div className="container">
+            <div className="leftSide">
+                {foods.every(food => food.count == 0) ? (
+
+                    <div className="text-empty"><p>Order list is Empty!</p></div>
+                ) : (
+                    foods.map(food => (
+                        food.count > 0 &&
+                        <div key={food.name}>
+                            <div>{food.name} x{food.count} - {food.price} KGS</div>
+                            <button className="xbtn" onClick={() => removeFood(food.name)}>X</button>
+
+                        </div>
+                    ))
+                )}
+                <div className="count">
+                   Total price:{getTotalPrice()} KGS
                 </div>
-            ))}
+            </div>
+            <div className="rightSide">
+                {menu.map(food => (
+                    <div key={food.name} className="buttons">
+                        <button onClick={() => addFood(food.name)}>{food.name} <br/>{food.price} KGS <img src={food.image}/>
+                        </button>
+
+                    </div>
+                ))}
+            </div>
+
         </div>
     );
 };
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,10 @@
-export interface food{
+export interface menuItem{
     name:string;
     price:number;
     image:string;
+}
+export interface Food{
+    name:string;
+    count:number;
+    price:number;
 }
